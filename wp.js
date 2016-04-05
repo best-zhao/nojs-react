18baf16518801e8c03e15247d99ac89985dc88db
nolure <nolure@vip.qq.com>
2016-04-05 14:37:34 +0800

    webpack

---
+++ wp.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack');
+var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
+module.exports = {
+  entry:{
+    index:[      
+      './examples/js/index.js'
+    ]
+  },
+  output: {
+    path: __dirname + '/examples/dist/',
+    publicPath: "/examples/dist/",
+    filename: '[name].bundle.js'
+  },
+  plugins: [ 
+    new CommonsChunkPlugin("seed.js")
+  ],
+  resolve: {
+    alias: {
+      nj: __dirname+'/lib'
+    }
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel?presets[]=react,presets[]=es2015'
+    }, {
+      test: /\.less/,
+      loader: 'style-loader!css-loader!less-loader'
+    }, {
+      test: /\.(css)$/,
+      loader: 'style-loader?insertAt=top!css-loader'
+    }, {
+      test: /\.(png|jpg)$/,
+      loader: 'url-loader?limit=8192'
+    }]
+  }
+};
