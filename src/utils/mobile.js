67cf9f24337bbba8024d61415184af7b52484879
nolure <nolure@vip.qq.com>
2018-01-19 09:16:39 +0800

    'push'

--- src/utils/mobile.js
+++ src/utils/mobile.js
@@ -44,7 +44,8 @@ if( !metaEl ){
         wrap.appendChild(metaEl)
         document.write(wrap.innerHTML)
     }
-    document.write('<style>body{font-size:'+12 * dpr + 'px}</style>')
+    var rem = getDeviceWidth()
+    document.write('<style>html{font-size:'+rem+'px} body{font-size:'+12 * dpr + 'px}</style>')
 }
 
 let tid
